49843bf7159770d59ea7659b0a33ec59c6c80d84
bvb-tech <jacktechfora@proton.me>
2023-11-19 17:14:10 +0100

    added intro and replaced instructions to bottom

--- index.php
+++ index.php
@@ -58,12 +58,25 @@ include ("recipies.php");
                 </div>
             </div>
         </div>
-        <div class="outlinedrecipe">
-            <div id="method">
-            <h3>instructions</h3>
-            <p><?php print $rows[0]['method'];?></></p>
-            </div>
+    <div class="outlinedrecipe">
+        <div id="method">
+        <h3>introduction</h3>
+        <p>The majority of us love to cook or at least try to cook some food, perhaps daily or on special occasions. We do keep recipes, some in the papers, some have organized books, some scribbled it on a convenient place like kitchen boards, some just permanently place them on their brains.
+
+Cooking is an art, food gratifies the audience when we cook with a secret ingredient called “Love”.
+
+I have not learned cooking, but loves to cook for my family. Like someone said, “I am an accidental home cook”.Picked up the skill gradually when I generate some interest in cooking. The level of satisfaction I get from cooking is unexplainable, especially when my family enjoys them with a big wide smile on their face.
+
+That smile gives me the confidence, that smile is my pillar and inspiration, that smile gives me the reason to venture into new recipes. There is a saying,</p>
         </div>
+    </div>
+
+    <div class="outlinedrecipe">
+        <div id="method">
+        <h3>instructions</h3>
+        <p><?php print $rows[0]['method'];?></></p>
+        </div>
+    </div>
         <div></div>
     
     </main>
