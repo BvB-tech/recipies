60e4933cc8f7f5e642edf51f60e9213bea8df888
bvb-tech <jacktechfora@proton.me>
2023-11-20 07:43:36 +0100

    removed empty lines added some tabs

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ include ("recipies.php");
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <header class="header">
-        
+    <header class="header">    
         <div class="menu">    
         <div id="logo"></div> 
         <div id="menu_item1">home</div>
@@ -60,17 +59,15 @@ include ("recipies.php");
         </div>
     <div class="introduction">
         <div id="method">
-        <h3>introduction</h3>
-        <p>The majority of us love to cook or at least try to cook some food, perhaps daily or on special occasions. We do keep recipes, some in the papers, some have organized books, some scribbled it on a convenient place like kitchen boards, some just permanently place them on their brains.
-
-Cooking is an art, food gratifies the audience when we cook with a secret ingredient called “Love”.
-
-I have not learned cooking, but loves to cook for my family. Like someone said, “I am an accidental home cook”.Picked up the skill gradually when I generate some interest in cooking. The level of satisfaction I get from cooking is unexplainable, especially when my family enjoys them with a big wide smile on their face.
-
-That smile gives me the confidence, that smile is my pillar and inspiration, that smile gives me the reason to venture into new recipes. There is a saying,</p>
+            <h3>introduction</h3>
+            <p>
+                The majority of us love to cook or at least try to cook some food, perhaps daily or on special occasions. We do keep recipes, some in the papers, some have organized books, some scribbled it on a convenient place like kitchen boards, some just permanently place them on their brains.
+                Cooking is an art, food gratifies the audience when we cook with a secret ingredient called “Love”.
+                I have not learned cooking, but loves to cook for my family. Like someone said, “I am an accidental home cook”.Picked up the skill gradually when I generate some interest in cooking. The level of satisfaction I get from cooking is unexplainable, especially when my family enjoys them with a big wide smile on their face.
+                That smile gives me the confidence, that smile is my pillar and inspiration, that smile gives me the reason to venture into new recipes. There is a saying.
+            </p>
         </div>
     </div>
-
     <div class="outlinedrecipe">
         <div id="method">
         <h3>instructions</h3>
@@ -78,7 +75,6 @@ That smile gives me the confidence, that smile is my pillar and inspiration, tha
         </div>
     </div>
         <div></div>
-    
     </main>
 
     <footer>
@@ -92,6 +88,5 @@ That smile gives me the confidence, that smile is my pillar and inspiration, tha
             </div>
         </div>
     </footer>
-
 </body>
 </html>
