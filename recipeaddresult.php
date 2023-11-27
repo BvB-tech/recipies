c46378c1f3abaf2e0547eed26f560b4e8354f7ab
bvb-tech <jacktechfora@proton.me>
2023-11-27 11:06:48 +0100

    updated styling in uniform format

--- recipeaddresult.php
+++ recipeaddresult.php
@@ -37,7 +37,12 @@ require_once ("AddRecipe.php");
             <div id="established">est and around since 1889</div>
             <div id="disclaimer">We're not responsible for burned food or bad tasting food</div>
             <div id="copyright">FoodCompany &copy; &nbsp; &nbsp; &nbsp; all rights reserved &nbsp; </div>
-            <div id="socials">facebook instagram x youtube</div>
+            <div class="socials">
+                    <img src="images/facebook_icon.png" alt="facebook">
+                    <img src="images/instagram_icon.png" alt="instagram">
+                    <img src="images/x_icon.png" alt="X">
+                    <img src="images/youtube_icon.png" alt="youtube">
+                </div>
         </div> 
     </footer>
 </body>
